f83e51e64bc9b0e731f8f3c3331ec3e0b17b21f5
“othaderek” <otha.hernandez@gmail.com>
2019-05-13 15:29:58 -0400

    testing

--- practice/scratch_pad.rb
+++ practice/scratch_pad.rb
@@ -1,29 +1,5 @@
-# Review of all Ruby concepts learned
-# This will include reviewing Learn Co problems
-
-# Linked List as a method
-# a = [1,2,3,4,5,6,7,8]
-
-# def insert(arr, item, pos)
-#   tmp = arr[pos]
-#   arr[pos] = item
-
-#   arr.replace(arr[0..pos] + [tmp] + arr[pos+1..-1])
-# end
-
-# insert(a, 204, 3)
-# p a
-
-
-i = 0
-arr = [1,2,3,4,5,6,7,8]
-
-while i < arr.length
-  puts "This is #{arr[i]}."
-
-  i += 1
-end
-
-arr.each do |i|
-  puts "This is #{i}"
-end
+arr = Array.new(100) { rand(1..100) }
+p arr
+puts
+arr = arr.sort
+p arr
